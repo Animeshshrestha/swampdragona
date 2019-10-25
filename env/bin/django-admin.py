7798828cb8e0672eda829358a16b5ba3d6fbf873
Anim92 <bharat.shrestha@infodev.tech>
2019-10-25 15:31:56 +0545

    all

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/suraj/Workspace/swampdragon-django-notifications-demo/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
